9e279478ef35a3e0ee905fadb46c447320ececfc
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-13 23:13:24 +0900

    物件情報登録時にバリデーションを定義

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,13 @@
 class Property < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :rent
+    validates :address
+    validates :age
+    validates :remarks
+  end
   has_many :nearest_stations, dependent: :destroy
-  accepts_nested_attributes_for :nearest_stations, allow_destroy: true,
-                                reject_if: :all_blank 
+  accepts_nested_attributes_for :nearest_stations, 
+                                 allow_destroy: true,
+                                 reject_if: :all_blank
 end
